541bceaf0b89b8885c209675996c3b3a224ca869
Alexis Villegas Torres <alexisvt@gmail.com>
2022-03-09 15:13:19 -0600

    Remove an implementation detail and add more realistic tests.

--- src/components/TodoList/__test__/TodoList.test.js
+++ src/components/TodoList/__test__/TodoList.test.js
@@ -1,12 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import TodoList from '../TodoList';
 
-const MockTodoList = ({ initialValue = [] }) => {
-  const [todos, setTodos] = useState(initialValue);
+const MockTodoList = ({ initialValue = [], mockStateUpdater = null }) => {
+  const todos = initialValue;
+  const setTodos = mockStateUpdater;
 
   return (
 
@@ -23,7 +23,7 @@ describe('TodoList', () => {
     expect(screen.getByText('test')).toBeInTheDocument()
   })
 
-  test('task should not have "todo-item-active" class initially', () => {
+  test('tasks should not have "todo-item-active" class initially', () => {
     render(<MockTodoList initialValue={[{ id: 1, task: 'test', completed: false }]} />)
 
     const todoItemElement = screen.getByText('test')
@@ -31,13 +31,22 @@ describe('TodoList', () => {
     expect(todoItemElement).not.toHaveClass('todo-item-active')
   })
 
-  test('Completed task should have "todo-item-active" class', () => {
-    render(<MockTodoList initialValue={[{ id: 1, task: 'test', completed: false }]} />)
+  test('should show "todo-item-active" class when task is completed', () => {
+    render(<MockTodoList initialValue={[{ id: 1, task: 'test', completed: true }]} />)
+
+    const todoItemElement = screen.getByText('test')
+
+    expect(todoItemElement).toHaveClass('todo-item-active')
+  })
+
+  test('should change uncompleted task status to complete when clicked', () => {
+    const mockFn = jest.fn();
+    render(<MockTodoList initialValue={[{ id: 1, task: 'test', completed: false }]} mockStateUpdater={mockFn} />)
 
     const todoItemElement = screen.getByText('test')
     userEvent.click(todoItemElement);
 
-    expect(todoItemElement).toHaveClass('todo-item-active')
-
+    expect(mockFn).toHaveBeenCalledTimes(1)
+    expect(mockFn).toHaveBeenCalledWith([{ id: 1, task: 'test', completed: true }])
   })
 })
